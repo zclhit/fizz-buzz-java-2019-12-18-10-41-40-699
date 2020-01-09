1d5d74cf621b82d5d885a5ef7852da133bc96349
changle.zhang <changle.c@thoughtworks.com>
2020-01-09 21:12:32 +0800

    [changle] #4: return Buzz when position can be exact divided by 5

--- src/main/java/com.thoughtworks/FizzBuzz.java
+++ src/main/java/com.thoughtworks/FizzBuzz.java
@@ -2,9 +2,16 @@ package com.thoughtworks;
 
 public class FizzBuzz {
     public String say(int position) {
-        if (position % 3 == 0) {
+        if (canBeExactedDividedBy(position, 3)) {
             return "Fizz";
         }
+        if (canBeExactedDividedBy(position, 5)) {
+            return "Buzz";
+        }
         return String.valueOf(position);
+    }
+
+    private boolean canBeExactedDividedBy(int position, int i) {
+        return position % i == 0;
     }
 }
--- src/test/java/com/thoughtworks/FIzzBuzzTest.java
+++ src/test/java/com/thoughtworks/FIzzBuzzTest.java
@@ -28,4 +28,14 @@ public class FIzzBuzzTest {
     public void should_return_number_of_position_given_position_can_not_be_exact_divided_by_3() {
         assertEquals("2", fizzBuzz.say(2));
     }
+
+    @Test
+    public void should_return_fizz_given_position_can_be_exacted_divided_by_3() {
+        assertEquals("Fizz", fizzBuzz.say(6));
+    }
+
+    @Test
+    public void should_return_buzz_given_position_can_be_exacted_divided_by_5() {
+        assertEquals("Buzz", fizzBuzz.say(5));
+    }
 }
